e46c1a186661dd01a3750f8e5012c7153e35d2ac
Lucas Marandat <lucas@dotmind.io>
2019-03-01 15:41:18 +0100

    Initial Commit

---
+++ epitech-calendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  epitech-calendar
+//
+//  Created by Lucas Marandat on 01/03/2019.
+//  Copyright © 2019 Lucas Marandat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
